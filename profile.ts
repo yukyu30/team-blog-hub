87deb2535fd92f08e65047154116d97a390174c0
yu5 <61819079+yukyu30@users.noreply.github.com>
2021-11-03 02:03:20 +0900

    プロフィールを追加

---
+++ profile.ts
@@ -0,0 +1,12 @@
+export const profile = {
+    name :{
+      ja:"悠久",
+      en:"yukyu",
+    },
+    twitterUsername: "yukyu30",
+    githubUsername: "yukyu30",
+    suzuriUsername: "yukyu_konpeki_dou",
+    website: "https://blog.yukyu.net",
+    bio: "広く浅くいろんなことに興味を持つ人間",
+    moredetails:"https://blog.yukyu.net/me/",
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 
 import posts from "@.contents/posts.json";
 import { config } from "@site.config";
+import {profile} from "@profile"
 import { PostItem } from "@src/types";
 import { ScrollableMembers } from "@src/components/ScrollableMembers";
 import { PostList } from "@src/components/PostList";
