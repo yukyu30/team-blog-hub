74625876e38b8a5e19d1d4088d90630854355d6b
yu5 <61819079+yukyu30@users.noreply.github.com>
2021-11-03 00:06:52 +0900

    blog をついか

--- members.ts
+++ members.ts
@@ -2,20 +2,14 @@ import { Member } from "@src/types";
 
 export const members: Member[] = [
   {
-    id: "catnose",
-    name: "CatNose",
-    role: "CTO",
+    id: "blog",
+    name: "ブログ",
     bio:
-      "デザインが好きなプログラマー。開発者向けの情報共有プラットフォームzenn.devを開発しています。",
-    avatarSrc: "/avatars/catnose.jpg",
+      "自分に関することを書いていくブログ",
+    avatarSrc: "/avatars/blog.png",
     sources: [
-      "https://zenn.dev/catnose99/feed",
-      "https://catnose.medium.com/feed",
+      "https://blog.yukyu.net/rss.xml",
     ],
-    includeUrlRegex: "medium.com|zenn.dev",
-    twitterUsername: "catnose99",
-    githubUsername: "catnose99",
-    websiteUrl: "https://catnose99.com",
   },
   {
     id: "john_doe",
