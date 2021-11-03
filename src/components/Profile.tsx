2a51d7cf4d35dede127c84761f565d77158a2d3f
yu5 <61819079+yukyu30@users.noreply.github.com>
2021-11-03 17:09:48 +0900

    記事を修正

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -7,14 +7,20 @@ export const ScrollableMembers: React.FC = () => {
     <div>
   
             <div className="member-header__avatar">
-              
+              <img
+                src={profile.avatarSrc}
+                alt={nprofile.name.ja}
+                width={100}
+                height={100}
+                className="member-header__avatar-img"
+              />
             </div>
-            <h1 className="member-header__name">{name.ja}</h1>
-            <p className="member-header__bio">{bio}</p>
+            <h1 className="member-header__name">{profile.name.ja}</h1>
+            <p className="member-header__bio">{profile.bio}</p>
             <div className="member-header__links">
               {twitterUsername && (
                 <a
-                  href={`https://twitter.com/${twitterUsername}`}
+                  href={`https://twitter.com/${profile.twitterUsername}`}
                   className="member-header__link"
                 >
                   <img
@@ -27,12 +33,12 @@ export const ScrollableMembers: React.FC = () => {
               )}
               {githubUsername && (
                 <a
-                  href={`https://github.com/${githubUsername}`}
+                  href={`https://github.com/${profile.githubUsername}`}
                   className="member-header__link"
                 >
                   <img
                     src="/icons/github.svg"
-                    alt={`GitHubのユーザー@${githubUsername}`}
+                    alt={`GitHubのユーザー@${profile.githubUsername}`}
                     width={22}
                     height={22}
                   />
@@ -40,19 +46,19 @@ export const ScrollableMembers: React.FC = () => {
               )}
               {suzuriUsername && (
                 <a
-                  href={`https://suzuri.jp/${suzuriUsername}`}
+                  href={`https://suzuri.jp/${profile.suzuriUsername}`}
                   className="member-header__link"
                 >
                   <img
                     src="/icons/suzuri.svg"
-                    alt={`suzuriのユーザー@${suzuriUsername}`}
+                    alt={`suzuriのユーザー@${profile.suzuriUsername}`}
                     width={22}
                     height={22}
                   />
                 </a>
               )}
               {websiteUrl && (
-                <a href={websiteUrl} className="member-header__link">
+                <a href={profile.websiteUrl} className="member-header__link">
                   <img
                     src="/icons/link.svg"
                     alt={`ウェブサイトのリンク`}
