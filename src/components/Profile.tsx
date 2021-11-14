0f1877ba583b7b8dc239a5530abbe20409acb60c
yu5 <61819079+yukyu30@users.noreply.github.com>
2021-11-14 15:28:15 +0900

    修正

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -5,79 +5,74 @@ import { getMemberPath } from "@src/utils/helper";
 
 export const Profile: React.FC = () => {
   return (
-    <div>
+    
       <div className ="profile__container">
 
-      <div className="profile__avatar">
-        <img
-          src={profile.avatarSrc}
-          alt={profile.name.ja}
-          className="profile__avatar-img"
-        />
-      </div>
+        <div className="profile__avatar">
+          <img
+            src={profile.avatarSrc}
+            alt={profile.name.ja}
+            className="profile__avatar-img"
+          />
+        </div>
 
-      <div className="profile__content">
-        <h1 className="profile__name">{profile.name.ja}</h1>
-        <p className="profile__bio">{profile.bio}</p>
-        <div className="profile__links">
-          {profile.twitterUsername && (
-            <a
-              href={`https://twitter.com/${profile.twitterUsername}`}
-              className="profile__link"
-            >
-              <img
-                src="/icons/twitter.svg"
-                alt={`Twitterのユーザー@${profile.twitterUsername}`}
-                width={22}
-                height={22}
-              />
-            </a>
-          )}
-          {profile.githubUsername && (
-            <a
-              href={`https://github.com/${profile.githubUsername}`}
-              className="profile__link"
-            >
-              <img
-                src="/icons/github.svg"
-                alt={`GitHubのユーザー@${profile.githubUsername}`}
-                width={22}
-                height={22}
-              />
-            </a>
-          )}
-          {profile.suzuriUsername && (
-            <a
-            　href={`https://suzuri.jp/${profile.suzuriUsername}`}
-              className="profile__link"
-            >
-              <img
-                src="/icons/suzuri.svg"
-                alt={`suzuriのユーザー@${profile.suzuriUsername}`}
-                width={22}
-                height={22}
-              />
-            </a>
-          )}
-          {profile.websiteUrl && (
-            <a href={profile.websiteUrl} className="profile__link">
-              <img
-                src="/icons/link.svg"
-                alt={`ウェブサイトのリンク`}
-                width={22}
-                height={22}
-              />
-            </a>
-          )}
+        <div className="profile__content">
+          <h1 className="profile__name">{profile.name.ja}</h1>
+          <p className="profile__bio">{profile.bio}</p>
+          <div className="profile__links">
+            {profile.twitterUsername && (
+              <a
+                href={`https://twitter.com/${profile.twitterUsername}`}
+                className="profile__link"
+              >
+                <img
+                  src="/icons/twitter.svg"
+                  alt={`Twitterのユーザー@${profile.twitterUsername}`}
+                  width={22}
+                  height={22}
+                />
+              </a>
+            )}
+            {profile.githubUsername && (
+              <a
+                href={`https://github.com/${profile.githubUsername}`}
+                className="profile__link"
+              >
+                <img
+                  src="/icons/github.svg"
+                  alt={`GitHubのユーザー@${profile.githubUsername}`}
+                  width={22}
+                  height={22}
+                />
+              </a>
+            )}
+            {profile.suzuriUsername && (
+              <a
+              　href={`https://suzuri.jp/${profile.suzuriUsername}`}
+                className="profile__link"
+              >
+                <img
+                  src="/icons/suzuri.svg"
+                  alt={`suzuriのユーザー@${profile.suzuriUsername}`}
+                  width={22}
+                  height={22}
+                />
+              </a>
+            )}
+            {profile.websiteUrl && (
+              <a href={profile.websiteUrl} className="profile__link">
+                <img
+                  src="/icons/link.svg"
+                  alt={`ウェブサイトのリンク`}
+                  width={22}
+                  height={22}
+                />
+              </a>
+            )}
+          </div>
         </div>
 
       </div>   
-    </div>
-
-      <div className ="profile__container">
-        <a href="https://blog-friends.com/users/yukyu30/" className="js-blog-friends" data-user="yukyu30">yukyu30</a>
-        <script async src="https://unpkg.com/blog-friends-embed@latest/widgets.js" charset="utf-8"></script>
-      </div>
-    </div>
+    
   );
 };
